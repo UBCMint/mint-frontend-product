6c8b2183413d5f49a0616c41e22e51c14186c108
yood2 <yood2@proton.me>
2025-01-26 07:41:10 -0800

    scaffold nextjs with shadcn and typography components

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { TypographyH1 } from '@/components/typography/typography';
+
+export default function Home() {
+    return (
+        <div>
+            <TypographyH1>MINT Frontend-Product</TypographyH1>
+        </div>
+    );
+}
