e4327a7cff38953517c40378378810f34496f553
Mostafa <mostafa.ali90001@gmail.com>
2025-02-02 15:08:33 -0800

    built resizbale sidebar that can be resized horizontally

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -1,18 +1,27 @@
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import Cross1 from '@/components/radix/cross1';
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 export default function SelectionSidebar() {
     return (
-        <div className="h-screen p-6 w-80">
-            <Card className="h-full">
-                <CardHeader className="flex flex-row items-center justify-between">
-                    <CardTitle>Nodes</CardTitle>
-                    <Cross1 />
-                </CardHeader>
-                <CardContent>
-                    <p>Content goes here</p>
-                </CardContent>
-            </Card>
-        </div>
+            <ResizablePanelGroup direction="horizontal" className="min-h-screen p-6">
+                <ResizablePanel className='min-h-full min-w-fit' defaultSize={1}>
+                    <Card className="h-full min-w-fit">
+                        <CardHeader className="flex flex-row items-center justify-between">
+                            <CardTitle>Nodes</CardTitle>
+                            <Cross1 />
+                        </CardHeader>
+                        <CardContent>
+                            <p>Content goes here</p>
+                        </CardContent>
+                    </Card>
+                </ResizablePanel>
+                <ResizableHandle withHandle className='bg-border-none'/>
+                <ResizablePanel defaultSize={99}/>
+            </ResizablePanelGroup>
     );
 }
