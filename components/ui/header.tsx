345b9f8669501f67b1d467b34ac5cbcd01f50d62
mcevoymicah <152262509+mcevoymicah@users.noreply.github.com>
2025-02-21 20:54:01 -0800

    added dropdown

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,29 +1,50 @@
 import { Button } from "@/components/ui/button";
-import { ExternalLinkIcon, ChevronUpIcon } from "@radix-ui/react-icons";
+import { ExternalLinkIcon, ChevronUpIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 export default function AppHeader() {
-  return (
-    <header className="flex justify-between items-center p-2 border-b h-16">
-      {/* logo */}
-      <div className="flex items-center">
-        <img src= "https://ubcmint.github.io/img/main-page/mint-logo.png" alt="Logo" className="h-14" />
-      </div>
+    return (
+        <header className="flex justify-between items-center p-2 border-b h-16">
+            {/* logo */}
+            <div className="flex items-center">
+                <img src="https://ubcmint.github.io/img/main-page/mint-logo.png" alt="Logo" className="h-14" />
+            </div>
 
-      {/* update, issues, help */}
-      <div className="flex items-center space-x-4">
-        <Button variant="link" className="flex items-center space-x-1 px-3 py-2">
-            <span>Update</span>
-            <ExternalLinkIcon className="h-4 w-4" />
-        </Button>
-        <Button variant="link" className="flex items-center space-x-1 px-3 py-2">
-          <span>Issues</span>
-          <ExternalLinkIcon className="h-4 w-4" />
-        </Button>
-        <Button variant="link" className="flex items-center space-x-1 px-3 py-2">
-          <span>Help</span>
-          <ChevronUpIcon className="h-4 w-4" />
-        </Button>
-      </div>
-    </header>
-  );
+            {/* update, issues */}
+            <div className="flex items-center space-x-4">
+                <Button variant="link" className="flex items-center space-x-1 px-3 py-2">
+                    <span>Update</span>
+                    <ExternalLinkIcon className="h-4 w-4" />
+                </Button>
+                <Button variant="link" className="flex items-center space-x-1 px-3 py-2">
+                    <span>Issues</span>
+                    <ExternalLinkIcon className="h-4 w-4" />
+                </Button>
+
+                {/* help */}
+                <DropdownMenu>
+                    <DropdownMenuTrigger><Button variant="link" className="flex items-center space-x-1 px-3 py-2">
+                        <span>Help</span>
+                        <ChevronUpIcon className="h-4 w-4" />
+                    </Button></DropdownMenuTrigger>
+                    <DropdownMenuContent className="mx-4">
+                        <DropdownMenuItem>Getting Started</DropdownMenuItem>
+                        <DropdownMenuItem>Testing Impendance</DropdownMenuItem>
+                        <DropdownMenuItem>Troubleshooting Guide</DropdownMenuItem>
+                        <DropdownMenuItem>Cyton Driver Fix</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            </div>
+
+        </header>
+    );
 }
