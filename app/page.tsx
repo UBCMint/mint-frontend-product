4873a20eb45a000eca2ed499f454c6fef87ed58c
yood2 <yood2@proton.me>
2025-02-22 09:54:15 -0800

    fix hydration error

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,6 @@ export default function Home() {
         <SettingsBar />
 
         <div className="p-8">
-            <TypographyH1>MINT Frontend-Product</TypographyH1>
-            
-            <div className="mt-8">
-                <RecentButton 
-                    label="Node Name" 
-                    description="Node Description Lorem Ipsum dolor sitamet Node Description Lorem Ipsum dolor sitamet"
-                />
-
-                <RecentButton 
-                    label="Node Name" 
-                    description="A different node description"
-                />
-            </div>
             </div>
         </div>
     );
